032def0ce2b6b75cab02c46aa25fb8cdcbc4095e
Fabio Ribeiro Martins <fabio.evangelion.guitar@gmail.com>
2020-07-12 10:42:27 -0300

    Screens created

---
+++ src/screens/Manage/Links/Edit/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Layout from '../../../Layouts/Manage';
+
+const Edit = () => {
+    return (
+        <Layout>
+            <h1>Edit Link</h1>
+            <div>
+                <form action="">
+                    <div className="form-group">
+                        <label htmlFor="">Label</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="">Url</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group form-check">
+                        <label className="form-check-label">
+                            <input type="checkbox" name="isSocial" />
+                            <span className="form-check-sign">Is Social</span>
+                        </label>
+                    </div>
+                    <div>
+                        <button className="btn btn-primary btn-round">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </Layout>
+    );
+};
+
+export default Edit;
